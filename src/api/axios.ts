422e4048adf7d5f2890904ae5e037e19ac4b9058
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-24 20:02:26 +0200

    fix: increase axios timeout

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const myAxios = axios.create({
     baseURL: 'http://localhost:8080/api',
-    timeout: 60_000,
+    timeout: 120_000,
     headers: {
         'Content-Type': 'application/json',
     },
